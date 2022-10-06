37fc5f8f0c99fe10ab1b661b8c803c535a30faba
arifimran5 <arifimran505@gmail.com>
2022-10-06 18:37:55 +0530

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thatarif.in',
   integrations: [
     tailwind({
       config: {
         applyBaseStyles: false,
       },
     }),
+    sitemap(),
+    robotsTxt(),
   ],
 });
